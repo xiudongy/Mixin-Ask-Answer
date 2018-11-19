052c1c6114a170819ee19659bdc810ffe99d5446
xiudongy <xiudongy@gmail.com>
2018-11-19 18:06:15 +0800

    add mixin oauth2

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'fla_',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => 'http://localhost:8089',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
